c854cbb994b8ce3e938362f1b4a15260028cffd8
robynainsley21 <robynainsley2i@gmail.com>
2024-05-21 12:46:14 +0200

    Make submission appear

--- script/script.js
+++ script/script.js
@@ -1,17 +1,18 @@
 /*submitted form box*/
-const submittedFormBox = document.getElementById('submitted-form')
+const submittedFormBox = document.getElementById("submitted-form");
 
 /*form elements*/
 const formElements = {
-  firstName: document.getElementById('firstName').value,
-  lastName: document.getElementById('lastName').value,
-  age: document.getElementById('age').value,
-  birthDate: document.getElementById('birthDate').value,
-  gender: document.querySelectorAll('[name=gender]').value,
-  title: document.querySelectorAll('[title]').value,
-  subject: document.querySelectorAll('[subject]'),
-  submitBtn: document.querySelector('[submit]')
-}
+  firstName: document.getElementById("firstName"),
+  lastName: document.getElementById("lastName"),
+  age: document.getElementById("age"),
+  birthDate: document.getElementById("birthDate"),
+  gender: document.querySelectorAll("[name=gender]"),
+  title: document.querySelectorAll("[title]"),
+  subject: document.querySelectorAll("[subject]"),
+  submitBtn: document.querySelector("[submit]"),
+  resetBtn: document.querySelector("[reset]")
+};
 
 /*select employment option*/
 const selectNo = document.getElementById("no");
@@ -44,41 +45,61 @@ selectNo.addEventListener("click", () => {
 });
 
 /*calculating salary*/
-calcItems.calcBtn.addEventListener('click', () => {
-    const hours = parseFloat(calcItems.hoursValue.value)
-    const rate = parseFloat(calcItems.rateValue.value)
-    let result = parseFloat(hours * rate)
+calcItems.calcBtn.addEventListener("click", () => {
+  const hours = parseFloat(calcItems.hoursValue.value);
+  const rate = parseFloat(calcItems.rateValue.value);
+  let result = parseFloat(hours * rate);
 
-    calcItems.totalOutput.value =  `R${result}`
-    console.log(result)
+  calcItems.totalOutput.value = `R${result}`;
+  console.log(result);
+});
+
+/*clearing submitted form*/
+formElements.resetBtn.addEventListener('click', () => {
+  submittedFormBox.innerHTML = ' '
 })
 
 /*submitting the form*/
-formElements.submitBtn.addEventListener('click', (event) => {
+submittedFormBox.innerHTML = ' '
+
+formElements.submitBtn.addEventListener("click", (event) => {
   //prevents form default behavior if inputs are invalid
-  event.preventDefault()
+  event.preventDefault();
 
-  let subjectsArray = []
+  let subjectsArray = [];
 
-  formElements.subject.forEach(item => {
-    if(item.checked){
-      subjectsArray.push(item.value)
+  formElements.subject.forEach((item) => {
+    if (item.checked) {
+      subjectsArray.push(item.value);
     }
-  })
+  });
 
-  // const selectedGender = [...formElements.gender].find(val => val.checked)?.value || 'Not specified'
-  const employmentStatus = selectYes.checked ? 'Employed' : 'Not employed'
+  const firstName = formElements.firstName.value
+  const lastName = formElements.lastName.value
+  const age = formElements.age.value
+  const birthDate = formElements.birthDate.value
+  const title = formElements.title.value
+  const selectedGender = [...formElements.gender].find(val => val.checked)?.value || 'Not specified'
+  const employmentStatus = selectYes.checked ? "Employed" : "Not employed"
 
   submittedFormBox.innerHTML = `
     <div>
       <h2>Form Details</h2>
-      <p>First name: ${formElements.firstName}</p>
-      <p>Last name: ${formElements.lastName}</p>
-      <p>Age: ${formElements.age}</p>
-      <p>Date of birth: ${formElements.birthDate}</p>
-      <p>Selected gender: ${formElements.gender? formElements.gender : 'None selected'}</p>
-      <p>Subject(s): ${subjectsArray.length > 0 ? subjectsArray.join(', ') : 'No subject selected'}</p>
+      <p>First name: ${firstName}</p>
+      <p>Last name: ${lastName}</p>
+      <p>Age: ${age}</p>
+      <p>Date of birth: ${birthDate}</p>
+      <p>Title: ${title}</p>
+      <p>Selected gender: ${
+        selectedGender
+        // formElements.gender.checked ? formElements.gender.checked : "None selected"
+      }</p>
+      <p>Subject(s): ${
+        subjectsArray.length > 0
+          ? subjectsArray.join(", ")
+          : "No subject selected"
+      }</p>
       <p>Employment status: ${employmentStatus}</p>
     </div>
-  `
-})
+  `;
+});
